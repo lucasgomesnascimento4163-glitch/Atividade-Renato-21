fd651c94876ba70bdf298049ed368fb6e043bee2
enriquecharao-ctrl <lucasgomesnascimento4163@gmail.com>
2026-09-21 15:08:43 -0400

    commit

---
+++ renato-main/agenda-pwa/sw.js
@@ -0,0 +1,18 @@
+self.addEventListener("install", event => {
+    console.log("Service Worker instalado");
+});
+
+self.addEventListener("activate", event => {
+    console.log("Service Worker ativado");
+});
+self.addEventListener("fetch", event => {
+    console.log("Requisição:", event.request.url)
+});
+self.addEventListener("fetch", (event) => {
+    if (event.request.url.endsWith("/teste-sw")) {
+        event.respondWith(new Response("Resposta criada pelo Service Worker!"));
+        return;
+    }
+});
+
+
